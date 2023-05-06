caf7fed4e9591c6c84da91323b8635f61b061470
ulko <ulianakochetkova00@gmail.com>
2023-05-06 18:50:29 +0300

    1) add swagger

--- src/main/java/com/moneyservice/MainController.java
+++ src/main/java/com/moneyservice/MainController.java
@@ -4,10 +4,7 @@ import com.moneyservice.model.BaseResponse;
 import com.moneyservice.services.MainService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
